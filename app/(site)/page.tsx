fb956d4c7be6c61e68abee8465fefc4b229d41cb
Harshit S <harshitks2203@gmail.com>
2023-06-19 22:41:28 -0400

    Layout - Added button component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,18 @@
+import Header from "@/components/Header";
+
 export default function Home() {
-  return <div className="text-green-500">Main content</div>;
+  return (
+    <div
+      className="
+      bg-neutral-900 
+        rounded-lg 
+        w-full 
+        h-full 
+        overflow-hidden 
+        overflow-y-auto
+      "
+    >
+      <Header>Header</Header>
+    </div>
+  );
 }
