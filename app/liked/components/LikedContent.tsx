c2c7d6db84192c6fee32d1368eb5330998cf8a47
Harshit S <harshitks2203@gmail.com>
2023-06-26 21:27:22 -0400

    Added player func to multiple components

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -7,12 +7,15 @@ import { Song } from "@/types";
 import { useUser } from "@/hooks/useUser";
 import MediaItem from "@/components/MediaItem";
 import LikeButton from "@/components/LikeButton";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface LikedContentProps {
   songs: Song[];
 }
 
 const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
+  const onPlay = useOnPlay(songs);
+
   const router = useRouter();
   const { isLoading, user } = useUser();
 
@@ -34,7 +37,7 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
       {songs.map((song: any) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem onClick={() => {}} data={song} />
+            <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
           </div>
           <LikeButton songId={song.id} />
         </div>
