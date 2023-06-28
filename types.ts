22284f5aad9fc1fb5a9a29c0944c2ab506a9e2f9
Harshit S <harshitks2203@gmail.com>
2023-06-27 21:21:07 -0400

    adding stripe integration

--- types.ts
+++ types.ts
@@ -36,7 +36,7 @@ export interface Price {
   currency?: string;
   type?: Stripe.Price.Type;
   interval?: Stripe.Price.Recurring.Interval;
-  inverval_count?: number;
+  interval_count?: number;
   trial_period_days?: number | null;
   metadata?: Stripe.Metadata;
   products?: Product;
